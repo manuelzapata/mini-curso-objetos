26e76ba89737728330860dd60d1b0b28d964c880
Manuel Ricardo Zapata <manuelricardozapata@gmail.com>
2019-06-05 11:57:30 -0500

    Inicio del ejemplo de herencia. Trabajo en progreso.

---
+++ herencia/coworking-java/src/main/java/course/oop/inheritance/Space.java
@@ -0,0 +1,13 @@
+package course.oop.inheritance;
+
+public abstract class Space {
+
+    private String name;
+    private int chairs;
+
+    public Space(String name, int chairs) {
+        this.name = name;
+        this.chairs = chairs;
+    }
+
+}
