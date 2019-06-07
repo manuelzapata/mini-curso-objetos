05b7324f81eb469a8b5dc6ddf19f05564b79621f
Manuel <manuel@blaucompany.co>
2019-06-07 16:19:31 -0500

    Ejemplo de polimorfismo con C#.

---
+++ polimorfismo/hr-csharp/Course.OOP.Polimorfism.HR/Entities/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course.OOP.Polimorfism.HR.Entities
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public double BaseSalary { get; set; }
+
+
+        public int CalculateVacationDays(DateTime endDate) {
+            Console.WriteLine("Método con parámetros");
+            int days = Math.Abs((endDate - StartDate).Days);
+            return days;
+        }
+
+        public int CalculateVacationDays() {
+            Console.WriteLine("Método sin parámetros");
+            int result = CalculateVacationDays(DateTime.Now);
+            return result;
+        }
+
+        public double CalculateNetSalary() {
+
+            Console.WriteLine("Estamos en la clase Employee");
+
+            const double socialSecurity = 0.08;
+
+            double netSalary = BaseSalary * (1 - socialSecurity);
+            return netSalary;
+
+        }
+
+
+    }
+}
---
+++ polimorfismo/hr-csharp/Course.OOP.Polimorfism.HR/Program.cs
@@ -0,0 +1,47 @@
+﻿using Course.OOP.Polimorfism.HR.Entities;
+using System;
+
+namespace Course.OOP.Polimorfism.HR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employeeOne = new Employee {
+                EmployeeID = 1,
+                FirstName = "Juan",
+                LastName = "De Las Nieves",
+                BaseSalary = 1000,
+                StartDate = new DateTime(2019, 01, 01)
+            };
+
+            Manager managerOne = new Manager {
+                EmployeeID = 2,
+                FirstName = "Lord",
+                LastName = "Commander",
+                BaseSalary = 10000,
+                Bonus = 1000,
+                StartDate = new DateTime(2008, 01, 01)
+            };
+
+            Employee employeeTwo = (Employee)managerOne;
+
+            //Polimorfismo por sobreescritura
+
+            Console.WriteLine("Salario employeeOne: " + employeeOne.CalculateNetSalary());
+            Console.WriteLine();
+
+            Console.WriteLine("Salario employeeTwo: " + employeeTwo.CalculateNetSalary());
+            Console.WriteLine();
+
+            Console.WriteLine("Salario managerOne: " + managerOne.CalculateNetSalary());
+            Console.WriteLine();
+
+            //Polimorfismo por sobrecarga
+
+            managerOne.CalculateVacationDays();
+
+            managerOne.CalculateVacationDays(new DateTime(2019, 1, 1));
+        }
+    }
+}
